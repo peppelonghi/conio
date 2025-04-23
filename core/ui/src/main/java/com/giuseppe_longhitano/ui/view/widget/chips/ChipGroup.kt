4d28236144684562b23895cefa2c92b1d43a4d54
peppelonghi <peppelonghitano@msn.com>
2025-04-23 09:09:39 +0200

    Clean

--- core/ui/src/main/java/com/giuseppe_longhitano/ui/view/widget/chips/ChipGroup.kt
+++ core/ui/src/main/java/com/giuseppe_longhitano/ui/view/widget/chips/ChipGroup.kt
@@ -1,21 +1,16 @@
 package com.giuseppe_longhitano.ui.view.widget.chips
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.FilterChip
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.giuseppe_longhitano.ui.view.shared.common_event.SelectionEvent
 import com.giuseppe_longhitano.ui.view.shared.common_ui_model.SelectableItem
@@ -47,3 +42,6 @@ fun <T> ChipGroup(
         }
     }
 }
+
+
+
